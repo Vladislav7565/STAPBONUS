20763a91ac0c955bc656696ab8251905fe4bf65b
BLAME BONUS <103824564+Vladislav7565@users.noreply.github.com>
2023-04-22 19:07:55 +0300

    Add files via upload

---
+++ profile/login.php
@@ -0,0 +1,131 @@
+<?php
+$string =  '<!DOCTYPE html>'.
+'	<html lang="ru">'.
+'	'.
+'<!-- Mirrored from r-rp.ru/profile/login by HTTrack Website Copier/3.x [XR&CO\'2014], Sat, 29 Sep 2018 18:06:57 GMT -->'.
+'<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->'.
+'<head>'.
+'		<meta charset="UTF-8">'.
+'		<meta name="viewport" content="width=device-width,initial-scale=1">'.
+'		<meta name="csrf-param" content="_csrf">'.
+'    <meta name="csrf-token" content="v59pv6fqaSjuL03n9IEHx8-HScAxsiOPqdvnEl2GPUaqicn54UHrDzuK4UCyYLPPePmJWn_tT2z88ULJ9ZTZ1A==">'.
+'		<title>Личный кабинет - Вход -STAP </title>'.
+'		<link href="../092018/css/bootstrap.min.css" rel="stylesheet">'.
+'<link href="../092018/css/style.min.css" rel="stylesheet">'.
+'<link rel="shortcut icon" href="./092018/images/logo.png" type="image/x-icon">	</head>'.
+'	<body>'.
+'					<div class="app">'.
+'				<div class="aaa">'.
+'					<nav class="navbar navbar-expand-lg navbar-light">'.
+'	<div class="container">'.
+'		<button data-toggle="collapse" data-target="#nav-collapse" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>'.
+'		<div class="navbar-brand">'.
+'			<div class="logotype logotype-navbar">'.
+'			<div class="logotype__image"></div>'.
+'			<div class="logotype__name">'.
+'				<div class="logotype__project">STAP</div>'.
+'				<div class="logotype__genre">Bonus</div>'.
+'			</div>'.
+'			</div>'.
+'			'.
+'			<a href="https://www.youtube.com/@macalister0222" target="_blank" class="navbar__button navbar__button--social navbar__button--yt"></a>'.
+'							<a href="login.html" class="btn btn--orange btn--navbar">Личный кабинет</a>'.
+'					</div>'.
+'		<div id="nav-collapse" class="collapse navbar-collapse">'.
+'			<ul class="navbar-nav">'.
+'				<li class="nav-item"><a href="../index.html" class="nav-link">Главная</a></li>'.
+'				<li class="nav-item"><a href="../news.html" class="nav-link">Новости</a></li>'.
+'				<li class="nav-item"><a href="http://forum.r-rp.ru/" class="nav-link">Форум</a></li>'.
+'				<li class="nav-item"><a href="../roulette.html" class="nav-link">Рулетка</a></li>'.
+'				<li class="nav-item"><a href="../pay.html" class="nav-link">Донат</a></li>'.
+'			</ul>'.
+'			<ul class="navbar-nav ml-auto buttons">'.
+'				'.
+'				<a href="https://www.youtube.com/@macalister0222" target="_blank" class="navbar__button navbar__button--social navbar__button--yt"></a>'.
+'									<a href="login.html" class="btn btn--orange btn--navbar">Личный кабинет</a>'.
+'							</ul>'.
+'		</div>'.
+'	</div>'.
+'</nav>					<div class="container">'.
+'	<div class="login">'.
+'		<div class="row">'.
+'			<div class="col-lg-4">'.
+'				<div class="block-title-2">Авторизация</div>'.
+'				<form id="login-form" action="http://r-rp.ru/profile/login" method="post">'.
+'<input type="hidden" name="_csrf" value="v59pv6fqaSjuL03n9IEHx8-HScAxsiOPqdvnEl2GPUaqicn54UHrDzuK4UCyYLPPePmJWn_tT2z88ULJ9ZTZ1A==">			        <div class=" field-loginform-server">'.
+'<div class=\'input-group\'><div class=\'input-icon input-icon--server\'></div><select id="loginform-server" class="input" name="LoginForm[server]">'.
+'<option value="0">Выберите сервер</option>'.
+'<option value="1">Сервер I</option>'.
+'<option value="2">Сервер II</option>'.
+'<option value="3">Сервер III</option>'.
+'<option value="4">Сервер IV</option>'.
+'</select><div class="form-error"></div></div>'.
+'</div>'.
+'					<div class=" field-loginform-username required">'.
+'<div class=\'input-group\'><div class=\'input-icon input-icon--nickname\'></div><input type="text" id="loginform-username" class="input" name="LoginForm[username]" placeholder="Введите никнейм" aria-required="true"><div class="form-error"></div></div>'.
+'</div>'.
+'			        <div class=" field-loginform-password required">'.
+'<div class=\'input-group\'><div class=\'input-icon input-icon--password\'></div><input type="password" id="loginform-password" class="input" name="LoginForm[password]" placeholder="Введите пароль" aria-required="true"><div class="form-error"></div></div>'.
+'</div>'.
+'					<div class="login__buttons">'.
+'						<button class="btn btn--enter" type="submit">Войти</button>'.
+'						<a class="btn btn--vk-2" href="https://oauth.vk.com/authorize?client_id=5837878&amp;scope=offline&amp;redirect_uri=http%3A%2F%2Fr-rp.ru%2Fvk%2Fauth&amp;v=5.29"></a>'.
+'					</div>'.
+'					<div class="login__recovery"><a href="../recovery/password.html">Забыл пароль?</a></div>            '.
+''.
+'			    </form>			</div>'.
+''.
+''.
+'			<div class="col-lg-8">'.
+'				<div class="block-title-2">Что нового в кабинете</div>'.
+'				<div class="new-in-cabinet">'.
+'					<div class="item">'.
+'						<div class="image img-1"></div>'.
+'						<div class="body">'.
+'							<div class="title">Новый дизайн личного кабинета</div>'.
+'							<div class="div"></div>'.
+'							<div class="content">Мы учли пожелания многих пользователей, и решили глобально обновить дизайн, перерисовали все страницы сайта, и сделали все очень простым и удобным.</div>'.
+'						</div>'.
+'					</div>'.
+'					<div class="item">'.
+'						<div class="image img-2"></div>'.
+'						<div class="body">'.
+'							<div class="title">Полное обновление интерфейса</div>'.
+'							<div class="div"></div>'.
+'							<div class="content">Наши разработчики решили вплотную заняться разработкой обновление, мы добавили реал-тайм статистику, изменили структуризацию отчетов, добавили удобные фишки</div>'.
+'						</div>'.
+'					</div>'.
+'				</div>'.
+'			</div>'.
+'		</div>'.
+'	</div>'.
+'</div>				</div>'.
+'				<div class="bbb">'.
+'					<footer class="footer">'.
+'	<div class="container">'.
+'		<div class="footer__wrap">'.
+'			<div class="logotype logotype-footer">'.
+'				<div class="logotype__image"></div>'.
+'				<div class="logotype__name">'.
+'					<div class="logotype__project">STAP</div>'.
+'					<div class="logotype__genre">Bonus</div>'.
+'				</div>'.
+'			</div>'.
+'		</div>'.
+'	</div>'.
+'</footer>				</div>'.
+''.
+'		<script src="../092018/js/jquery.min.js"></script>'.
+'<script src="../assets/9522e78d/yii.js"></script>'.
+'<script src="../assets/9522e78d/yii.validation.js"></script>'.
+'<script src="../assets/9522e78d/yii.activeForm.js"></script>'.
+'<script src="../092018/js/bootstrap.bundle.min.js"></script>'.
+'<script type="text/javascript">jQuery(document).ready(function () {'.
+'jQuery(\'#login-form\').yiiActiveForm([{"id":"loginform-server","name":"server","container":".field-loginform-server","input":"#loginform-server","error":".form-error","validate":function (attribute, value, messages, deferred, $form) {yii.validation.number(value, messages, {"pattern":/^\\s*[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?\\s*$/,"message":"Значение «Сервер» должно быть числом.","min":1,"tooSmall":"Выберите сервер","max":4,"tooBig":"Не корректный сервер","skipOnEmpty":1});}},{"id":"loginform-username","name":"username","container":".field-loginform-username","input":"#loginform-username","error":".form-error","validate":function (attribute, value, messages, deferred, $form) {yii.validation.required(value, messages, {"message":"Введите Никнейм"});}},{"id":"loginform-password","name":"password","container":".field-loginform-password","input":"#loginform-password","error":".form-error","validate":function (attribute, value, messages, deferred, $form) {yii.validation.required(value, messages, {"message":"Введите Пароль"});}}], []);'.
+'});</script>	</body>'.
+'	'.
+'<!-- Mirrored from r-rp.ru/profile/login by HTTrack Website Copier/3.x [XR&CO\'2014], Sat, 29 Sep 2018 18:07:01 GMT -->'.
+'</html>';
+
+ echo $string; 
+?>
